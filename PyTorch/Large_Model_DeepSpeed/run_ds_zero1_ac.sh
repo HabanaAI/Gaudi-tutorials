7b2219697ed4440ee7deb1dff7f8dbaaf905cbd1
Greg Serochi <56761205+greg-serochi@users.noreply.github.com>
2023-02-21 11:34:49 -0800

    Updated DS version and Activation Checkpointing

--- PyTorch/Large_Model_DeepSpeed/run_ds_zero1_ac.sh
+++ PyTorch/Large_Model_DeepSpeed/run_ds_zero1_ac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-deepspeed demo_ds.py --deepspeed --deepspeed_config ds_config_zero1_ac.json --use_hpu --activation-checkpoint $@
+EXPERIMENTAL_WEIGHT_SHARING=0 deepspeed demo_ds.py --deepspeed --deepspeed_config ds_config_zero1_ac.json --use_hpu --activation-checkpoint $@
