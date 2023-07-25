7110f58a6aaec49ac0d4f36f6e0fad74afc4d76f
Greg Serochi <56761205+greg-serochi@users.noreply.github.com>
2023-07-25 11:31:28 -0700

    Create dyn_ops3.py

---
+++ PyTorch/Detecting_Dynamic_Shapes/dyn_ops3.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2023, Habana Labs Ltd.  All rights reserved.
+
+import torchvision, os
+from PIL import Image
+import torchvision.transforms as T
+
+import habana_frameworks.torch.core as htcore
+device = 'hpu'
+
+#load model
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model.eval() # set to evaluation mode
+model = model.to(device) # move model to device
+
+from habana_frameworks.torch.utils.experimental import detect_recompilation_auto_model
+model = detect_recompilation_auto_model(model, waittime=0.3)
+
+for idx, k in enumerate(os.listdir('coco128/images/train2017/')):
+    img = Image.open('coco128/images/train2017/' + k).resize((600,600))
+    img = T.ToTensor()(img).to(device)
+    print('inp shape:', img.shape)
+    pred = model([img])
+    htcore.mark_step()
+    if idx == 6: # just running first few images
+        break
+    print('done img', idx)
+model.analyse_dynamicity()
+
