a9bcd11d3d87d9eed97939fddd34dd1b30bc495a
Mohit Deopujari <mdeopujari@habana.ai>
2025-03-10 19:06:36 -0700

    Updates vllm step

---
+++ PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_model.sh
@@ -0,0 +1,32 @@
+# Check if a model name is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <model_name>"
+  exit 1
+fi
+
+# Get the model name from the argument
+MODEL_NAME="$1"
+
+# Construct the checkpoint path
+CHECKPOINT_PATH="/root/models/$MODEL_NAME/"
+
+# Update package lists and install git-lfs
+apt-get update
+apt-get install -y git-lfs
+
+# Install git-lfs
+git lfs install
+
+# Clone the repository
+git clone "https://huggingface.co/$MODEL_NAME" "$CHECKPOINT_PATH"
+
+echo "Model $MODEL_NAME cloned to $CHECKPOINT_PATH"
+
+
+
+#cd /root
+#export CHECKPOINT_PATH=/root/models/Meta-Llama-3.1-8B-Instruct/
+#apt-get update
+#apt-get install git-lfs
+#git lfs install
+#git clone https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct $CHECKPOINT_PATH
