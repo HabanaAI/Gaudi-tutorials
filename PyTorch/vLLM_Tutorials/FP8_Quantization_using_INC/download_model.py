cd6990fe6f09108f9bacac1369f57c6005823b59
Mohit Deopujari <mdeopujari@habana.ai>
2025-03-14 17:31:39 -0700

    Fixed version

--- PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_model.py
+++
@@ -1,4 +0,0 @@
-import os
-from huggingface_hub import snapshot_download
-
-snapshot_download(repo_id=os.environ['MODEL_NAME'], local_dir=os.environ['CHECKPOINT_PATH'], local_dir_use_symlinks=False, token=os.environ['HF_TOKEN'])
--- PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_model.sh
+++
@@ -1,2 +0,0 @@
-echo "Downloading Model $MODEL_NAME to $CHECKPOINT_PATH"
-python3 download_model.py
