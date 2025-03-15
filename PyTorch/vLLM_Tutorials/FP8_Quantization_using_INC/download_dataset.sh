f3b797120142c7ec61bc9ecb75afb688d330880f
Mohit Deopujari <mdeopujari@habana.ai>
2025-03-14 17:31:39 -0700

    Final commit

--- PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_dataset.sh
+++ PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_dataset.sh
@@ -1,6 +1,6 @@
 wget https://rclone.org/install.sh
 chmod a+x ./install.sh
-./install.sh
+bash install.sh
 
 rclone config create mlc-inference s3 provider=Cloudflare access_key_id=f65ba5eef400db161ea49967de89f47b secret_access_key=fbea333914c292b854f14d3fe232bad6c5407bf0ab1bebf78833c2b359bdfd2b endpoint=https://c2686074cb2caf5cbaf6d134bdba8b47.r2.cloudflarestorage.com
 
--- PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_model.sh
+++ PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_model.sh
@@ -1,23 +1,2 @@
-# Check if a model name is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <model_name>"
-  exit 1
-fi
-
-# Get the model name from the argument
-MODEL_NAME="$1"
-
-# Construct the checkpoint path
-CHECKPOINT_PATH="/root/models/$MODEL_NAME/"
-
-# Update package lists and install git-lfs
-apt-get update
-apt-get install -y git-lfs
-
-# Install git-lfs
-git lfs install
-
-# Clone the repository
-git clone "https://huggingface.co/$MODEL_NAME" "$CHECKPOINT_PATH"
-
-echo "Model $MODEL_NAME cloned to $CHECKPOINT_PATH"
+echo "Downloading Model $MODEL_NAME to $CHECKPOINT_PATH"
+python3 download_model.py
