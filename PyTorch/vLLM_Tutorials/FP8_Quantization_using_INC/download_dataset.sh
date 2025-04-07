9dc6c0ccec187a60c048b63a76ffcdc9d1c5410a
Mohit Deopujari <mdeopujari@habana.ai>
2025-04-07 11:29:26 -0700

    FP8 quantization example on vLLM (#115)
    * FP8 Quantization using INC
    
    * Updates vllm step
    
    * Updates vllm step
    
    * before test on IDC
    
    * Final commit
    
    * Fixed version
    
    * Update vLLM_Benchmark_Serving.ipynb
    
    Removed line added here by mistake,

---
+++ PyTorch/vLLM_Tutorials/FP8_Quantization_using_INC/download_dataset.sh
@@ -0,0 +1,9 @@
+wget https://rclone.org/install.sh
+chmod a+x ./install.sh
+bash install.sh
+
+rclone config create mlc-inference s3 provider=Cloudflare access_key_id=f65ba5eef400db161ea49967de89f47b secret_access_key=fbea333914c292b854f14d3fe232bad6c5407bf0ab1bebf78833c2b359bdfd2b endpoint=https://c2686074cb2caf5cbaf6d134bdba8b47.r2.cloudflarestorage.com
+
+rclone copy mlc-inference:mlcommons-inference-wg-public/open_orca /root/open_orca -P
+
+gzip -c -d /root/open_orca/open_orca_gpt4_tokenized_llama.sampled_24576.pkl.gz > /root/open_orca/open_orca_gpt4_tokenized_llama.sampled_24576.pkl
